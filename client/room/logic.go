0dfd02a3673029ada823f04dcba5847be9e64205
termer <termer@termer.net>
2026-02-15 17:44:09 -0500

    refactor MessageHandlers into Logic interface so that it can better interface with ServerShareManager

--- client/room/logic.go
+++ client/room/logic.go
@@ -2,17 +2,37 @@ package room
 
 import (
 	"context"
-	"time"
 
+	"friendnet.org/client/share"
 	"friendnet.org/protocol"
 	pb "friendnet.org/protocol/pb/v1"
 )
 
-// MessageHandlersImpl implements all handlers in MessageHandlers.
-var MessageHandlersImpl = MessageHandlers{
-	S2cOnPing: func(ctx context.Context, room *Conn, bidi protocol.ProtoBidi, msg *protocol.TypedProtoMsg[*pb.MsgPing]) error {
-		return bidi.Write(pb.MsgType_MSG_TYPE_PONG, &pb.MsgPong{
-			SentTs: time.Now().UnixMilli(),
-		})
-	},
+// Logic exposes handlers for incoming client messages, both S2C and C2C.
+//
+// Each handler is provided with the information it needs to return a response.
+// Handlers must not hold references to the bidi or connection outside the handler.
+// Handlers do not need to close bidis; they are closed by the caller after the handler returns.
+type Logic interface {
+	// OnPing handles an incoming ping request.
+	//
+	// S2C, C2C
+	OnPing(ctx context.Context, room *Conn, bidi protocol.ProtoBidi, msg *protocol.TypedProtoMsg[*pb.MsgPing]) error
+}
+
+// LogicImpl implements Logic.
+type LogicImpl struct {
+	shares *share.ServerShareManager
+}
+
+var _ Logic = (*LogicImpl)(nil)
+
+func NewLogic(shares *share.ServerShareManager) *LogicImpl {
+	return &LogicImpl{
+		shares: shares,
+	}
+}
+
+func (l *LogicImpl) OnPing(_ context.Context, _ *Conn, bidi protocol.ProtoBidi, _ *protocol.TypedProtoMsg[*pb.MsgPing]) error {
+	return bidi.Write(pb.MsgType_MSG_TYPE_PONG, &pb.MsgPong{})
 }
