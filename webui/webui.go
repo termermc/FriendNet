ea8692e748964edfba1e0527a790e2d762f53fd4
termer <termer@termer.net>
2026-02-17 13:53:06 -0500

    Create client RPC schema

--- webui/webui.go
+++ webui/webui.go
@@ -15,6 +15,6 @@ var Dist embed.FS
 
 // TODO On the future client RPC, ensure the CORS is set to prevent websites from interacting with the RPC.
 
-// TODO On the future client RPC, require an authorization header that's generated on startup or specified with a flag.
+// TODO On the future client RPC, require an authorization header that's generated if not already in the client database (with a flag to regenerate it).
 // This token will be included in the web UI URL that is opened in the browser and will be consumed by the webapp during startup to know how to authenticate.
 // During startup, the RPC URL should be included in the web UI URL as well, so that it knows how to reach it.
