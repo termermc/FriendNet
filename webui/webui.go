a6c7fd66bd24e098162a7c6838394e2a3bd7079f
termer <termer@termer.net>
2026-02-16 19:25:15 -0500

    Add some future TODOs

--- webui/webui.go
+++ webui/webui.go
@@ -9,3 +9,12 @@ import "embed"
 //
 //go:embed dist/*
 var Dist embed.FS
+
+// TODO Add functions to launch a webserver that serves the web UI.
+// TODO Ensure CSP headers are set that prevent any inline or external anything.
+
+// TODO On the future client RPC, ensure the CORS is set to prevent websites from interacting with the RPC.
+
+// TODO On the future client RPC, require an authorization header that's generated on startup or specified with a flag.
+// This token will be included in the web UI URL that is opened in the browser and will be consumed by the webapp during startup to know how to authenticate.
+// During startup, the RPC URL should be included in the web UI URL as well, so that it knows how to reach it.
