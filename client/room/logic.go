42c0d27a688326116b390dba556ff1d47d4ff4d2
termer <termer@termer.net>
2026-02-28 18:39:51 -0500

    disallow direct connections in OnConnectToMe if direct connections are disabled

--- client/room/logic.go
+++ client/room/logic.go
@@ -281,6 +281,12 @@ func (l *LogicImpl) OnGetFile(_ context.Context, _ *Conn, bidi C2cBidi, msg *pro
 }
 
 func (l *LogicImpl) OnConnectToMe(ctx context.Context, room *Conn, bidi C2cBidi, _ *protocol.TypedProtoMsg[*pb.MsgConnectToMe]) error {
+	if room.directMgr.IsDisabled() {
+		return bidi.Write(pb.MsgType_MSG_TYPE_DIRECT_CONN_RESULT, &pb.MsgDirectConnResult{
+			Result: pb.ConnResult_CONN_RESULT_DID_NOT_TRY,
+		})
+	}
+
 	_, result, err := room.tryConnectToPeer(ctx, bidi.Username)
 	if err != nil && result == pb.ConnResult_CONN_RESULT_INTERNAL_ERROR {
 		room.logger.Error("internal error while connecting to peer",
