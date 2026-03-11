2426abfc8b80a3d7c03545e459d300b0b9722d41
termer <termer@termer.net>
2026-03-11 15:50:36 -0400

    Create DownloadManager struct. It will exist at a higher level than MultiClient, and be used by the RPC.

---
+++ client/downloadmanager.go
@@ -0,0 +1,34 @@
+package client
+
+import (
+	"context"
+	"sync"
+
+	"friendnet.org/client/event"
+	"friendnet.org/client/storage"
+)
+
+const dlDirIncompleteSetting = "download_dir_incomplete"
+const dlDirCompleteSetting = "download_dir_complete"
+
+// DownloadManager manages downloads across multiple servers.
+// It can resume and retry downloads, even when the client is closed and reopened, or when a peer goes offline and
+// comes back later.
+// It is designed to work similarly to the download manager in Nicotine+.
+//
+// In the completed folder, the directory structure is as follows:
+// `/<peer username>-<server UUID>/<peer path>...`
+//
+// So if you download "/music/song.mp3" from "jimmy" on server "abcd1234", the file will be saved at path:
+// `/jimmy-abcd1234/music/song.mp3`
+type DownloadManager struct {
+	mu       sync.RWMutex
+	isClosed bool
+
+	ctx       context.Context
+	ctxCancel context.CancelFunc
+
+	multi    *MultiClient
+	eventBus *event.Bus
+	storage  *storage.Storage
+}
