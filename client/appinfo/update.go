5dbf534d9d780864f6c25b1cf35896f43fb29f57
termer <termer@termer.net>
2026-03-10 23:18:09 -0400

    update to 1.0.1

--- client/appinfo/update.go
+++ client/appinfo/update.go
@@ -5,8 +5,8 @@ import "friendnet.org/common/updater"
 // CurrentUpdate is the current update the client is running.
 // If the current update fetched from an online source has a timestamp before this one, it must be ignored.
 var CurrentUpdate = updater.UpdateInfo{
-	CreatedTs:   1773027199,
-	Version:     "1.0.0",
-	Description: "Initial release of FriendNet.",
-	Url:         "https://github.com/termermc/FriendNet/releases/tag/v1.0.0",
+	CreatedTs:   1773198933,
+	Version:     "1.0.1",
+	Description: " - Improved client UI\n - Fixed issue with opening new browser windows for already-running clients\n - Added -rmcerthost client flag to remove hostnames from the certificate store (needed if a server changes certificate)\n - Added Linux ARM64 build",
+	Url:         "https://github.com/termermc/FriendNet/releases/tag/v1.0.1",
 }
