4ed9ce593a6d2ea20822df3a7905b1b544191a3f
termer <termer@termer.net>
2026-03-27 23:29:26 -0400

    make remove buttons functional on UI

--- webui/src/page/TransfersPage.tsx
+++ webui/src/page/TransfersPage.tsx
@@ -1,75 +1,173 @@
 import styles from './TransfersPage.module.css'
 
-import { Component, createMemo, JSX, Match, onMount, Show, Switch } from 'solid-js'
-import { useGlobalState } from '../ctx'
+import {
+	Component,
+	createMemo,
+	For,
+	JSX,
+	Match,
+	onMount,
+	Show,
+	Switch,
+} from 'solid-js'
+import { useGlobalState, useRpcClient } from '../ctx'
 import { formatSize, formatSpeed, makeBrowsePath } from '../util'
 import { Download } from '../transfer'
 import { A } from '@solidjs/router'
 import { DownloadStatus } from '../../pb/clientrpc/v1/rpc_pb'
+import { RpcClient } from '../protobuf'
+import { Code, ConnectError } from '@connectrpc/connect'
 
-const DownloadItem: Component<{ item: Download }> = (props) => {
-	const filename = props.item.filePath.substring(props.item.filePath.lastIndexOf('/') + 1)
+async function doRemove(client: RpcClient, uuid: string): Promise<void> {
+	try {
+		await client.removeDownloadManagerItem({ uuid })
+	} catch (err) {
+		if (err instanceof ConnectError) {
+			if (err.code !== Code.NotFound) {
+				console.error('failed to remove item:', err)
+				alert('Failed to remove item: ' + err.message)
+				return
+			}
+		} else {
+			console.error('failed to remove item:', err)
+			alert('Failed to remove item, check console for details')
+			return
+		}
+	}
+}
+
+const DownloadFolder: Component<{
+	server: Download['server']
+	peerUsername: Download['peerUsername']
+	containingDir: string
+	items: Download[]
+}> = (props) => {
+	const client = useRpcClient()
+	const items = props.items
+
+	async function removeAll() {
+		for (const item of items) {
+			await doRemove(client, item.uuid)
+		}
+	}
 
 	return (
-		<div classList={{
-			[styles.transfer]: true,
-			[styles.canceled]: props.item.status() === DownloadStatus.CANCELED,
-			[styles.done]: props.item.status() === DownloadStatus.DONE,
-			[styles.pending]: props.item.status() === DownloadStatus.PENDING,
-			[styles.queued]: props.item.status() === DownloadStatus.QUEUED,
-			[styles.error]: props.item.status() === DownloadStatus.ERROR,
-		}}>
+		<div class={styles.itemContainer}>
+			<button
+				onClick={removeAll}
+				title="Remove (does not remove files on disk)"
+			>🗑️</button>{' '}
+			<A
+				href={makeBrowsePath(
+					props.server.uuid,
+					props.peerUsername,
+					'/',
+				)}
+				class={styles.peer}
+			>
+				{'👤 '}
+				{props.peerUsername}
+				{' @ '}
+				{props.server.name()}
+			</A>
+			<A
+				href={makeBrowsePath(
+					props.server.uuid,
+					props.peerUsername,
+					props.containingDir,
+				)}
+				class={styles.path}
+			>
+				{'📁 '}
+				{props.containingDir}
+			</A>
+			<div class={styles.items}>
+				<For each={items}>
+					{(item) => <DownloadItem item={item} />}
+				</For>
+			</div>
+		</div>
+	)
+}
+
+const DownloadItem: Component<{ item: Download }> = (props) => {
+	const client = useRpcClient()
+
+	const item = props.item
+
+	const filename = item.filePath.substring(
+		item.filePath.lastIndexOf('/') + 1,
+	)
+
+	return (
+		<div
+			classList={{
+				[styles.transfer]: true,
+				[styles.canceled]:
+					item.status() === DownloadStatus.CANCELED,
+				[styles.done]: item.status() === DownloadStatus.DONE,
+				[styles.pending]:
+					item.status() === DownloadStatus.PENDING,
+				[styles.queued]: item.status() === DownloadStatus.QUEUED,
+				[styles.error]: item.status() === DownloadStatus.ERROR,
+			}}
+		>
 			<div class={styles.info}>{filename}</div>
 			<div class={styles.progress}>
-				<progress value={props.item.downloadedBytes() / props.item.fileSizeBytes()} max="1"/>
+				<progress
+					value={
+						item.downloadedBytes() /
+						item.fileSizeBytes()
+					}
+					max="1"
+				/>
 				<div class={styles.options}>
 					<button
+						onClick={() => doRemove(client, item.uuid)}
 						title="Remove (does not remove files on disk)"
 					>
 						🗑️
-					</button>
-					{' '}
-
+					</button>{' '}
 					<Switch>
-						<Match when={props.item.status() === DownloadStatus.CANCELED}>
-							<button
-								title="Retry"
-							>
-								🔄
-							</button>
+						<Match
+							when={item.status() === DownloadStatus.CANCELED}
+						>
+							<button title="Retry">🔄</button>
 						</Match>
-						<Match when={props.item.status() === DownloadStatus.DONE}>
+						<Match
+							when={item.status() === DownloadStatus.DONE}
+						>
 							<b>Done</b>
 						</Match>
-						<Match when={props.item.status() === DownloadStatus.PENDING}>
-							<button
-								title="Cancel"
-							>
-								⛔
-							</button>
+						<Match
+							when={item.status() === DownloadStatus.PENDING}
+						>
+							<button title="Cancel">⛔</button>
 						</Match>
-						<Match when={props.item.status() === DownloadStatus.QUEUED}>
-							<button
-								title="Download Now"
-							>
-								➡️
-							</button>
+						<Match
+							when={item.status() === DownloadStatus.QUEUED}
+						>
+							<button title="Download Now">➡️</button>
 						</Match>
-						<Match when={props.item.status() === DownloadStatus.ERROR}>
+						<Match
+							when={item.status() === DownloadStatus.ERROR}
+						>
 							<span class={styles.errorMessage}>
-								Error: {props.item.errorMessage()}
+								Error: {item.errorMessage()}
 							</span>
 						</Match>
 					</Switch>
 				</div>
 				{' | '}
 				<div class={styles.stats}>
-					{formatSize(props.item.downloadedBytes(), 2)}
+					{formatSize(item.downloadedBytes(), 2)}
 					{' / '}
-					{props.item.fileSizeBytes() === -1 ? '???' : formatSize(props.item.fileSizeBytes(), 2)}
-					<Show when={props.item.status() !== DownloadStatus.DONE}>
+					{item.fileSizeBytes() === -1
+						? '???'
+						: formatSize(item.fileSizeBytes(), 2)}
+					<Show when={item.status() !== DownloadStatus.DONE}>
 						{' | '}
-						{formatSpeed(props.item.lastSpeedBytesPerSecond())}
+						{formatSpeed(item.lastSpeedBytesPerSecond())}
 					</Show>
 				</div>
 			</div>
@@ -111,43 +209,19 @@ export const TransfersPage: Component = () => {
 			server: Download['server']
 			peerUsername: Download['peerUsername']
 			containingDir: string
-			items: JSX.Element[]
+			items: Download[]
 		}
 		let lastContainer: Container | null = null
 
 		const flushContainer = () => {
 			if (lastContainer != null && lastContainer.items.length > 0) {
 				elems.push(
-					<div class={styles.itemContainer}>
-						<A
-							href={makeBrowsePath(
-								lastContainer.server.uuid,
-								lastContainer.peerUsername,
-								'/',
-							)}
-							class={styles.peer}
-						>
-							{'👤 '}
-							{lastContainer.peerUsername}
-							{' @ '}
-							{lastContainer.server.name()}
-						</A>
-						<A
-							href={makeBrowsePath(
-								lastContainer.server.uuid,
-								lastContainer.peerUsername,
-								lastContainer.containingDir,
-							)}
-							class={styles.path}
-						>
-							{'📁 '}
-							{lastContainer.containingDir}
-						</A>
-
-						<div class={styles.items}>
-							{lastContainer.items}
-						</div>
-					</div>
+					<DownloadFolder
+						server={lastContainer.server}
+						peerUsername={lastContainer.peerUsername}
+						containingDir={lastContainer.containingDir}
+						items={lastContainer.items}
+					/>,
 				)
 			}
 		}
@@ -160,7 +234,10 @@ export const TransfersPage: Component = () => {
 			const serverUuid = item.server.uuid
 			const peerUsername = item.peerUsername
 			const filePath = item.filePath
-			const containingDir = filePath.substring(0, filePath.lastIndexOf('/'))
+			const containingDir = filePath.substring(
+				0,
+				filePath.lastIndexOf('/'),
+			)
 
 			const hash = `${serverUuid}:${peerUsername}:${containingDir}`
 			let lastHash: string | null = null
@@ -179,7 +256,7 @@ export const TransfersPage: Component = () => {
 				}
 			}
 
-			lastContainer.items.push(<DownloadItem item={item}/>)
+			lastContainer.items.push(item)
 		}
 
 		flushContainer()
