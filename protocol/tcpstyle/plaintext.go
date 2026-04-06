6f59ec9a81f71fc44a8fc6161585d439dd47fbc4
termer <termer@termer.net>
2026-04-06 19:32:38 -0400

    plaintext quic emulation progress

--- protocol/tcpstyle/plaintext.go
+++ protocol/tcpstyle/plaintext.go
@@ -2,6 +2,8 @@ package tcpstyle
 
 import (
 	"context"
+	"crypto/rand"
+	"encoding/binary"
 	"errors"
 	"io"
 	"log/slog"
@@ -25,17 +27,28 @@ type AcceptFunc func() (net.Conn, error)
 
 type streamOp uint8
 
+// Do not remove or rearrange streamOp values, or the protocol will break!
 const (
 	streamOpOpenConn streamOp = iota
-	streamOpAcceptConn
-	streamOpRejectConn
-	streamCloseConn
+	streamOpCloseConn
 	streamOpOpenBidi
 )
 
 type streamHeader struct {
 	ConnId int64
 	Op     streamOp
+}
+
+func readStreamHeader(r io.Reader) (streamHeader, error) {
+	var header streamHeader
+	err := binary.Read(r, binary.LittleEndian, &header)
+	if err != nil {
+		return streamHeader{}, err
+	}
+	return header, nil
+}
+func writeStreamHeader(w io.Writer, header streamHeader) error {
+	return binary.Write(w, binary.LittleEndian, header)
 }
 
 // ConnManager manages connections and streams for TCP-style plaintext connections.
@@ -80,7 +93,7 @@ func NewConnManager(
 
 	go func() {
 		for !m.isClosed {
-			conn, err := m.accept()
+			rawConn, err := m.accept()
 			if err != nil {
 				m.logger.Error("failed to accept connection",
 					"service", "tcpstyle.ConnManager",
@@ -88,7 +101,7 @@ func NewConnManager(
 				)
 				continue
 			}
-			if conn == nil {
+			if rawConn == nil {
 				_ = m.Close()
 				return
 			}
@@ -98,8 +111,60 @@ func NewConnManager(
 
 			// Handle connection.
 			go func() {
-				// TODO Read header, figure out if it's a new or existing connection.
-				_ = conn
+				header, err := readStreamHeader(rawConn)
+				if err != nil {
+					_ = rawConn.Close()
+					return
+				}
+
+				switch header.Op {
+				case streamOpOpenConn:
+					// Connection ID should be 0 here.
+					if header.ConnId != 0 {
+						_ = rawConn.Close()
+						return
+					}
+
+					// Generate a connection ID.
+					var idBytes [8]byte
+					_, _ = rand.Read(idBytes[:])
+					id := int64(binary.LittleEndian.Uint64(idBytes[:]))
+
+					// Wait for the connection to be accepted.
+					c := newConn(m, rawConn.RemoteAddr())
+					m.acceptCh <- c
+
+					// Connection accepted, add to map and write success.
+					m.mu.Lock()
+					m.conns[id] = c
+					m.mu.Unlock()
+
+					_, _ = rawConn.Write(idBytes[:])
+					_ = rawConn.Close()
+				case streamOpCloseConn:
+					m.mu.RLock()
+					c, has := m.conns[header.ConnId]
+					m.mu.RUnlock()
+					if !has {
+						_ = rawConn.Close()
+						return
+					}
+					_ = c.CloseWithReason("closed by peer")
+				case streamOpOpenBidi:
+					m.mu.RLock()
+					c, has := m.conns[header.ConnId]
+					m.mu.RUnlock()
+					if !has {
+						_ = rawConn.Close()
+						return
+					}
+
+					// Pass raw connection to conn to be used as a bidi stream.
+					c.sendBidi(rawConn)
+				default:
+					// Unknown op.
+					_ = rawConn.Close()
+				}
 			}()
 		}
 	}()
@@ -123,44 +188,100 @@ func (m *ConnManager) Close() error {
 }
 
 // Dial makes a new outgoing connection to the specified address.
-func (m *ConnManager) Dial(addr string) (net.Conn, error) {
-	// TODO implement me
-	panic("implement me")
+func (m *ConnManager) Dial(addr string) (*Conn, error) {
+	openConn, err := m.dial(addr)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		_ = openConn.Close()
+	}()
+
+	err = writeStreamHeader(openConn, streamHeader{
+		ConnId: 0,
+		Op:     streamOpOpenConn,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// Read connection ID.
+	var idBytes [8]byte
+	_, err = io.ReadFull(openConn, idBytes[:])
+	if err != nil {
+		return nil, err
+	}
+	id := int64(binary.LittleEndian.Uint64(idBytes[:]))
+
+	conn := newConn(m, openConn.RemoteAddr())
+
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	_, has := m.conns[id]
+	if has {
+		// The server sent an ID we already have.
+		// In the real world, this should never happen, so the server implementation must be doing something weird.
+		// We'll just drop the connection and let it time out on the other end.
+		return nil, errors.New("server sent duplicate connection ID")
+	}
+
+	// Connection succeeded; add to map and return.
+	m.conns[id] = conn
+	return conn, nil
 }
 
+// Conn is an implementation of protocol.ProtoConn that uses TCP-style plaintext connections under the hood to emulate
+// QUIC bidi streams.
 type Conn struct {
+	mu sync.RWMutex
+
+	m    *ConnManager
 	addr net.Addr
 
-	mu sync.RWMutex
+	incomingBidiCh chan net.Conn
 }
 
 var _ protocol.ProtoConn = (*Conn)(nil)
 
-func (t *Conn) RemoteAddr() net.Addr {
-	return t.addr
+func newConn(m *ConnManager, addr net.Addr) *Conn {
+	return &Conn{
+		m:    m,
+		addr: addr,
+
+		incomingBidiCh: make(chan net.Conn),
+	}
 }
 
-func (t *Conn) CloseWithReason(s string) error {
+func (c *Conn) sendBidi(conn net.Conn) {
+	c.incomingBidiCh <- conn
+}
+
+func (c *Conn) RemoteAddr() net.Addr {
+	return c.addr
+}
+
+func (c *Conn) CloseWithReason(s string) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (t *Conn) OpenBidiWithMsg(typ pb.MsgType, msg proto.Message) (bidi protocol.ProtoBidi, err error) {
+func (c *Conn) OpenBidiWithMsg(typ pb.MsgType, msg proto.Message) (bidi protocol.ProtoBidi, err error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (t *Conn) WaitForBidi(ctx context.Context) (protocol.ProtoBidi, error) {
+func (c *Conn) WaitForBidi(ctx context.Context) (protocol.ProtoBidi, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (t *Conn) SendAndReceive(typ pb.MsgType, msg proto.Message) (*protocol.UntypedProtoMsg, error) {
+func (c *Conn) SendAndReceive(typ pb.MsgType, msg proto.Message) (*protocol.UntypedProtoMsg, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (t *Conn) SendAndReceiveAck(typ pb.MsgType, msg proto.Message) error {
+func (c *Conn) SendAndReceiveAck(typ pb.MsgType, msg proto.Message) error {
 	//TODO implement me
 	panic("implement me")
 }
