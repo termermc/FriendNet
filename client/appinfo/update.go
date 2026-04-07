e78e3b391beb00d03b1b60156a156f69a2c03b12
termer <termer@termer.net>
2026-04-07 12:59:32 -0400

    bump version to 1.1.2

--- client/appinfo/update.go
+++ client/appinfo/update.go
@@ -5,8 +5,8 @@ import "friendnet.org/common/updater"
 // CurrentUpdate is the current update the client is running.
 // If the current update fetched from an online source has a timestamp before this one, it must be ignored.
 var CurrentUpdate = updater.UpdateInfo{
-	CreatedTs:   1775266955,
-	Version:     "1.1.1",
-	Description: "This is a bug fix release.\n\nChanges:\n - Fixed downloads not re-queuing when a peer goes offline mid-download\n - Fixed the transfers page failing to render when an empty file download was queued\n - Web UI render errors are now logged and displayed\n\nA news section was also added to the FriendNet website. You can subscribe to its RSS feed if you would like to keep up with FriendNet development news.",
-	Url:         "https://github.com/termermc/FriendNet/releases/tag/v1.1.1",
+	CreatedTs:   1775580848,
+	Version:     "1.1.2",
+	Description: "This update improves the server management experience. If you do not run a server, you can skip this update.\n\nChanges:\n - Added built-in CLI to server (RPC client is no longer required to manage it)\n - Added bearer token support to RPC client",
+	Url:         "https://github.com/termermc/FriendNet/releases/tag/v1.1.2",
 }
