e74c14863662d71c26717377251ab59e05964796
termer <termer@termer.net>
2026-04-09 03:31:39 -0400

    update to 1.1.3

--- updater/update.go
+++ updater/update.go
@@ -1,10 +1,10 @@
 package updater
 
-// CurrentUpdate is the current update the client is running.
+// CurrentUpdate is the current update the program is running.
 // If the current update fetched from an online source has a timestamp before this one, it must be ignored.
 var CurrentUpdate = UpdateInfo{
-	CreatedTs:   1775580848,
-	Version:     "1.1.2",
-	Description: "This update improves the server management experience. If you do not run a server, you can skip this update.\n\nChanges:\n - Added built-in CLI to server (RPC client is no longer required to manage it)\n - Added bearer token support to RPC client",
-	Url:         "https://github.com/termermc/FriendNet/releases/tag/v1.1.2",
+	CreatedTs:   1775719750,
+	Version:     "1.1.3",
+	Description: "This release adds an admin UI to the server and fixes a client bug.\n\nChanges:\n - Added an optional admin web UI to manage servers\n - Fixed online user list not loading correctly when reconnecting to a server",
+	Url:         "https://github.com/termermc/FriendNet/releases/tag/v1.1.3",
 }
