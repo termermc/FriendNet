2a79f9ffb1aae66cf7b7f7a4972223f29007f386
termer <termer@termer.net>
2026-07-25 04:25:33 -0400

    bump version to 1.2.0

--- updater/update.go
+++ updater/update.go
@@ -3,8 +3,8 @@ package updater
 // CurrentUpdate is the current update the program is running.
 // If the current update fetched from an online source has a timestamp before this one, it must be ignored.
 var CurrentUpdate = UpdateInfo{
-	CreatedTs:   1775719750,
-	Version:     "1.1.3",
-	Description: "This release adds an admin UI to the server and fixes a client bug.\n\nChanges:\n - Added an optional admin web UI to manage servers\n - Fixed online user list not loading correctly when reconnecting to a server",
-	Url:         "https://github.com/termermc/FriendNet/releases/tag/v1.1.3",
+	CreatedTs:   1784967355,
+	Version:     "1.2.0",
+	Description: "This release adds NAT hole punching for clients and improves the file browse view.\n\nBoth servers and clients need to update to take advantage of NAT hole punching. To learn more about it, visit https://friendnet.org/news/nat-hole-punching/.\nAs usual, servers and clients are backwards- and forwards-compatible, so all existing features will continue to work regardless of whether servers or clients update.\n\nChanges:\n - Added NAT hole punching for peer direct connections.\n - Added file size column to files table\n - Added pagination for large folders",
+	Url:         "https://github.com/termermc/FriendNet/releases/tag/v1.2.0",
 }
