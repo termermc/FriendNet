1b778cb2294feecbcd32df95f6c60d91560ede96
termer <termer@termer.net>
2026-08-12 22:13:33 -0400

    bump version to 1.2.2

--- updater/update.go
+++ updater/update.go
@@ -3,8 +3,8 @@ package updater
 // CurrentUpdate is the current update the program is running.
 // If the current update fetched from an online source has a timestamp before this one, it must be ignored.
 var CurrentUpdate = UpdateInfo{
-	CreatedTs:   1786220253,
-	Version:     "1.2.1",
-	Description: "This release makes a number of improvements to the FriendNet client and server, including new features for both.\n\nThis release was done in collaboration with a new contributor, @vwxi. I want to thank him for working with me on many features in this release.\n\nChanges:\n - Added the ability to search all connected servers at once\n - Fixed some file browser sizing issues\n - Fixed connecting to yourself using the network\n - Added quick relay fallback for some kinds of actions to improve latency\n - Direct connections to use are now sorted by lowest latency\n - Added external authentication for servers (see: friendnet.org/docs/server/external-authentication)\n - Removed the need for HTTPS on the web UI (root is no longer needed because a certificate no longer needs to be installed!)\n - Added the ability to do keyword blocking for search serverside (see: friendnet.org/docs/server/search-filtering/)\n - Added AppImage builds for Linux\n\nI'm very excited to work on the next release, which will be bigger than this one.",
-	Url:         "https://github.com/termermc/FriendNet/releases/tag/v1.2.1",
+	CreatedTs:   1786587091,
+	Version:     "1.2.2",
+	Description: "This release fixes a few bugs for the client and server. Updating is recommended.\n\nChanges:\n - Fixed direct connections not being removed when they disconnect\n - Fixed some annoying console errors related to [http-over-websocket](https://github.com/termermc/http-over-websocket)\n - Fixed server HTTP external auth rejecting correct `Content-Type` response header when it contained a charset field\n - Fixed server HTTP external auth doubling its request headers on every request\n - Fixed PDFs being previewed as raw text\n - Added a limit of 1MiB for previewing files as text",
+	Url:         "https://github.com/termermc/FriendNet/releases/tag/v1.2.2",
 }
